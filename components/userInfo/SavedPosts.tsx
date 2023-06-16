7c1991102350c3a66238490b3728f3b18a9e3c35
david emioma <davidemiomauche@gmail.com>
2023-06-16 23:44:04 +0100

    deployment issues

--- components/userInfo/SavedPosts.tsx
+++ components/userInfo/SavedPosts.tsx
@@ -9,13 +9,19 @@ interface Props {
 }
 
 const SavedPosts = ({ bookmarks }: Props) => {
+  const getPost = (id: string) => {
+    const post = usePostById(id);
+
+    return post;
+  };
+
   return (
     <>
       {bookmarks.length > 0 ? (
         <div className="w-full max-w-[calc(1024px-64px)] mx-auto">
           <div className="grid grid-cols-3 gap-1 sm:gap-5 md:gap-8">
             {bookmarks.map((bookmark) => {
-              const post = usePostById(`${bookmark?.id}`);
+              const post = getPost(`${bookmark?.id}`);
 
               if (!post) return null;
 
